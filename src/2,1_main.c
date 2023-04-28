71e6915ae4d3763a51fa9abab246f540510d0533
DennisDick <122461822+DennisDick@users.noreply.github.com>
2023-04-28 09:16:57 +0000

    Vorlesung 3

---
+++ src/2,1_main.c
@@ -0,0 +1,20 @@
+
+
+#include "2,1_student.h"
+
+int main(void)
+{
+    student max, maria;
+
+    student_init(&max, "Max Mustermann");
+    student_add_grade(&max, 1.2f);
+    student_add_grade(&max, 4.3f);
+    student_add_grade(&max, 1.4f);
+
+    student_init(&maria, "Maria Musterfrau");
+    student_add_grade(&maria, 5.0f);
+    student_add_grade(&maria, 1.0f);
+
+    student_print(&max);
+    student_print(&maria);
+}
